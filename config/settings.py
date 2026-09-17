86a972e89ef1d41e4b93f9d53e94278dfd00ece4
Nando-Suka <jorginando5@gmail.com>
2026-09-17 16:29:47 +0700

    initial commit

--- config/settings.py
+++ config/settings.py
@@ -60,9 +60,12 @@ TIME_ZONE = "Asia/Jakarta"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "images"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_URL = "login"
